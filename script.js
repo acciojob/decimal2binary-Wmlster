623db0a30017b09df8d306d4197b0bcabf86450a
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2024-06-19 22:12:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+
+	let s="";
+
+	while(num!=0)
+		{
+			s+=num%2;
+			num/=2;
+		}
+
+	return s;
 }
 
 window.decimalToBinary = decimalToBinary;
